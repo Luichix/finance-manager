aa1e94d18daec945693055e462e3b6fa462cbbe8
Luichix <luichix.rex@gmail.com>
2024-02-12 11:31:37 -0300

    feat(config): implement tailwind and nextui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		react({
-			include: ['**/react/*']
-		}),
-		tailwind({
-			// Example: Provide a custom path to a Tailwind config file
-			configFile: './tailwind.config.mjs'
-		})
-	]
+  integrations: [
+    react({
+      include: ["**/react/*"],
+    }),
+    tailwind(),
+  ],
 });
+
+// {
+// 	// Example: Provide a custom path to a Tailwind config file
+// 	configFile: './tailwind.config.mjs'
+// }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,14 @@
-// tailwind.config.cjs
-const { nextui } = require('@nextui-org/react');
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: [
-		// ...
-		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
-	],
-	theme: {
-		extend: {}
-	},
-	darkMode: 'class',
-	plugins: [nextui()]
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
 };
