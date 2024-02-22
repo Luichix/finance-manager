f7bfbee2b0bb80913305ccdd8ab89032411e468c
LucasAgui <lucasagui2205@gmail.com>
2024-02-22 15:43:28 -0300

    feat(categories): array of categories added in the Dashboard home

---
+++ frontend/src/components/FormDashboard/categories.ts
@@ -0,0 +1,89 @@
+const CATEGORIES: Record<string, string | boolean>[] = [
+  {
+    name: "Suscripciones",
+    isIncome: false,
+    id: "subscriptions",
+  },
+  {
+    name: "Publicidad",
+    isIncome: false,
+    id: "publishing",
+  },
+  {
+    name: "Compras",
+    isIncome: false,
+    id: "shopping",
+  },
+  {
+    name: "Servicios",
+    isIncome: false,
+    id: "services",
+  },
+  {
+    name: "Juegos",
+    isIncome: false,
+    id: "games	",
+  },
+  {
+    name: "Educación",
+    isIncome: false,
+    id: "education",
+  },
+  {
+    name: "Salud",
+    isIncome: false,
+    id: "health",
+  },
+  {
+    name: "Finanzas",
+    isIncome: false,
+    id: "finances",
+  },
+  {
+    name: "Viajes",
+    isIncome: false,
+    id: "travel",
+  },
+  {
+    name: "Estilo de vida",
+    isIncome: false,
+    id: "lifeStyle",
+  },
+  {
+    name: "Productividad",
+    isIncome: false,
+    id: "productivity",
+  },
+  {
+    name: "Entretenimiento",
+    isIncome: false,
+    id: "entertainment",
+  },
+  {
+    name: "Social",
+    isIncome: false,
+    id: "social",
+  },
+  {
+    name: "Noticias",
+    isIncome: false,
+    id: "news",
+  },
+  {
+    name: "Ejercicio",
+    isIncome: false,
+    id: "exercise",
+  },
+  {
+    name: "Música",
+    isIncome: false,
+    id: "music",
+  },
+  {
+    name: "Libros",
+    isIncome: false,
+    id: "books",
+  },
+];
+
+export { CATEGORIES };
