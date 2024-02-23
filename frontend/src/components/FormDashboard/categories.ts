221f34faaa4ae5dc605ef636e2825bc42ce968f8
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2024-02-22 21:45:23 -0300

    feat(transactions): Read categories from categories.ts. Still mock data

--- frontend/src/components/FormDashboard/categories.ts
+++ frontend/src/components/FormDashboard/categories.ts
@@ -1,88 +1,137 @@
-const CATEGORIES: Record<string, string | boolean>[] = [
+import type { IconType } from "react-icons";
+import {
+  IoLogoUsd,
+  IoSwapHorizontalOutline,
+  IoBagCheckOutline,
+  IoHomeOutline,
+} from "react-icons/io5";
+
+const CATEGORIES: Record<string, string | boolean | number | IconType>[] = [
   {
     name: "Suscripciones",
     isIncome: "OUTCOME",
     id: "subscriptions",
+    icon: IoLogoUsd,
   },
   {
     name: "Publicidad",
     isIncome: "OUTCOME",
     id: "publishing",
+    icon: IoLogoUsd,
   },
   {
     name: "Compras",
     isIncome: "OUTCOME",
     id: "shopping",
+    icon: IoLogoUsd,
   },
   {
     name: "Servicios",
     isIncome: "OUTCOME",
     id: "services",
+    icon: IoLogoUsd,
   },
   {
     name: "Juegos",
     isIncome: "OUTCOME",
     id: "games	",
+    icon: IoLogoUsd,
   },
   {
     name: "Educación",
     isIncome: "OUTCOME",
     id: "education",
+    icon: IoLogoUsd,
   },
   {
     name: "Salud",
     isIncome: "OUTCOME",
     id: "health",
+    icon: IoLogoUsd,
   },
   {
     name: "Finanzas",
     isIncome: "OUTCOME",
     id: "finances",
+    icon: IoLogoUsd,
   },
   {
     name: "Viajes",
     isIncome: "OUTCOME",
     id: "travel",
+    icon: IoLogoUsd,
   },
   {
     name: "Estilo de vida",
     isIncome: "OUTCOME",
     id: "lifeStyle",
+    icon: IoLogoUsd,
   },
   {
     name: "Productividad",
     isIncome: "OUTCOME",
     id: "productivity",
+    icon: IoLogoUsd,
   },
   {
     name: "Entretenimiento",
     isIncome: "OUTCOME",
     id: "entertainment",
+    icon: IoLogoUsd,
   },
   {
     name: "Social",
     isIncome: "OUTCOME",
     id: "social",
+    icon: IoLogoUsd,
   },
   {
     name: "Noticias",
     isIncome: "OUTCOME",
     id: "news",
+    icon: IoLogoUsd,
   },
   {
     name: "Ejercicio",
     isIncome: "OUTCOME",
     id: "exercise",
+    icon: IoLogoUsd,
   },
   {
     name: "Música",
     isIncome: "OUTCOME",
     id: "music",
+    icon: IoLogoUsd,
   },
   {
     name: "Libros",
     isIncome: "OUTCOME",
     id: "books",
+    icon: IoLogoUsd,
+  },
+  {
+    name: "Alquiler",
+    isIncome: false,
+    id: 4,
+    icon: IoHomeOutline,
+  },
+  {
+    name: "Supermercado",
+    isIncome: false,
+    id: 3,
+    icon: IoBagCheckOutline,
+  },
+  {
+    name: "Sueldo",
+    isIncome: true,
+    id: 2,
+    icon: IoLogoUsd,
+  },
+  {
+    name: "Transferencia",
+    isIncome: true,
+    id: 1,
+    icon: IoSwapHorizontalOutline,
   },
   {
     name: "Pagos",
