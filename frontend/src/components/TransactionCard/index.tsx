5c1518944e566287e9606d176d2437b0e87717f6
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2024-02-26 00:36:32 -0300

    feat(transactions): Add view/hide description details on TransactionCard

--- frontend/src/components/TransactionCard/index.tsx
+++ frontend/src/components/TransactionCard/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import Icon from "@/components/Icon/Index";
+import React, { useState } from 'react';
+import { FaMoneyCheckAlt } from 'react-icons/fa';
+import Icon from '@/components/Icon/Index';
 
 interface TransactionCardProps {
   categoryId: number;
@@ -14,26 +15,37 @@ const TransactionCard: React.FC<TransactionCardProps> = ({
   amount,
   description,
 }) => {
-  const formattedDate = new Date(date).toLocaleDateString("es-ES", {
-    day: "numeric",
-    month: "short",
-    year: "2-digit",
+  const formattedDate = new Date(date).toLocaleDateString('es-ES', {
+    day: 'numeric',
+    month: 'short',
+    year: '2-digit',
   });
+
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
 
   return (
     <div className="transaction-card flex items-center justify-between mx-4 p-4 shadow-md rounded">
       <div className="column">
-        <Icon id={"finances"} />
+        <Icon id={'finances'} />
       </div>
       <div className="column">
         <p className="font-semibold">{`Categoria: ${categoryId}`}</p>
         <p>{formattedDate}</p>
       </div>
       <div className="column text-lg font-bold">
-        <p>{amount !== null ? `$${amount.toFixed(2)}` : "N/A"}</p>
-      </div>
-      <div className="column">
-        <p>{description}</p>
+        <p>{amount !== null ? `$${amount.toFixed(2)}` : 'N/A'}</p>
+        {showDetails ? (
+          <div>
+            <p>{description}</p>
+            <button onClick={toggleDetails}>Ocultar Detalle</button>
+          </div>
+        ) : (
+          <button onClick={toggleDetails}>Ver Detalle</button>
+        )}
       </div>
     </div>
   );
